043eb9f60b7e5a5c7ae99b8d20a93a6b76263929
shekarcp <shekarcp@yahoo.com>
2019-08-22 11:34:42 -0400

    Update main_fed.py

--- main_fed.py
+++ main_fed.py
@@ -9,6 +9,12 @@ import copy
 import numpy as np
 from torchvision import datasets, transforms
 import torch
+import random
+import codecs
+import json
+import pickle
+import keras
+import uuid
 
 from utils.sampling import mnist_iid, mnist_noniid, cifar_iid
 from utils.options import args_parser
@@ -16,6 +22,10 @@ from models.Update import LocalUpdate
 from models.Nets import MLP, CNNMnist, CNNCifar
 from models.Fed import FedAvg
 from models.test import test_img
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
 
 
 # python main_fed.py --dataset mnist --num_channels 1 --model cnn --epochs 50 --gpu 0 
@@ -90,3 +100,90 @@ if __name__ == '__main__':
     print("Training accuracy: {:.2f}".format(acc_train))
     print("Testing accuracy: {:.2f}".format(acc_test))
 
+    class GlobalModel(object):
+    """docstring for GlobalModel"""
+    def __init__(self):
+        self.model = self.build_model()
+        self.current_weights = self.model.get_weights()
+        # for convergence check
+        self.prev_train_loss = None
+        
+        # all rounds; losses[i] = [round#, timestamp, loss]
+        # round# could be None if not applicable
+        self.train_losses = []
+        self.valid_losses = []
+        self.train_accuracies = []
+        self.valid_accuracies = []
+
+        self.training_start_time = int(round(time.time()))
+    
+    def build_model(self):
+        raise NotImplementedError()
+        
+    def update_weights(self, client_weights, client_sizes):
+        new_weights = [np.zeros(w.shape) for w in self.current_weights]
+        total_size = np.sum(client_sizes)
+
+        for c in range(len(client_weights)):
+            for i in range(len(new_weights)):
+                new_weights[i] += client_weights[c][i] * client_sizes[c] / total_size
+        self.current_weights = new_weights
+        
+    def aggregate_loss_accuracy(self, client_losses, client_accuracies, client_sizes):
+        total_size = np.sum(client_sizes)
+        # weighted sum
+        aggr_loss = np.sum(client_losses[i] / total_size * client_sizes[i]
+                for i in range(len(client_sizes)))
+        aggr_accuraries = np.sum(client_accuracies[i] / total_size * client_sizes[i]
+                for i in range(len(client_sizes)))
+        return aggr_loss, aggr_accuraries
+      
+    def aggregate_train_loss_accuracy(self, client_losses, client_accuracies, client_sizes, cur_round):
+        cur_time = int(round(time.time())) - self.training_start_time
+        aggr_loss, aggr_accuraries = self.aggregate_loss_accuracy(client_losses, client_accuracies, client_sizes)
+        self.train_losses += [[cur_round, cur_time, aggr_loss]]
+        self.train_accuracies += [[cur_round, cur_time, aggr_accuraries]]
+        with open('stats.txt', 'w') as outfile:
+            json.dump(self.get_stats(), outfile)
+        return aggr_loss, aggr_accuraries
+      
+    def aggregate_valid_loss_accuracy(self, client_losses, client_accuracies, client_sizes, cur_round):
+        cur_time = int(round(time.time())) - self.training_start_time
+        aggr_loss, aggr_accuraries = self.aggregate_loss_accuracy(client_losses, client_accuracies, client_sizes)
+        self.valid_losses += [[cur_round, cur_time, aggr_loss]]
+        self.valid_accuracies += [[cur_round, cur_time, aggr_accuraries]]
+        with open('stats.txt', 'w') as outfile:
+            json.dump(self.get_stats(), outfile)
+        return aggr_loss, aggr_accuraries
+      
+    def get_stats(self):
+        return {
+            "train_loss": self.train_losses,
+            "valid_loss": self.valid_losses,
+            "train_accuracy": self.train_accuracies,
+            "valid_accuracy": self.valid_accuracies
+        }
+      
+class GlobalModel_MNIST_CNN(GlobalModel):
+    def __init__(self):
+        super(GlobalModel_MNIST_CNN, self).__init__()
+
+    def build_model(self):
+        # ~5MB worth of parameters
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=(28, 28, 1)))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(10, activation='softmax'))
+
+        model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adadelta(),
+                      metrics=['accuracy'])
+        return model
+print("hi")
